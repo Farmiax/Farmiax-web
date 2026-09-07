e08afdf958e858dbc7dfe11fe4c79a70eb5451eb
Farmiax <Farmiax107@gmail.com>
2026-09-08 03:14:17 +0530

    Add allowedHosts for server and preview configurations

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['farmiax-evps.onrender.com'],
+  },
+
+  preview: {
+    allowedHosts: ['farmiax-evps.onrender.com'],
+  },
 })
