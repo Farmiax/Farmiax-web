adbdc58c0ae2150de5874af908e7871ee09b7f1e
Farmiax <Farmiax107@gmail.com>
2026-09-12 14:03:30 +0530

    Refactor getFarmerProducts to use object shorthand

--- Frontend/src/services/productService.js
+++ Frontend/src/services/productService.js
@@ -70,9 +70,11 @@ const productService = {
   getFarmerProducts: async (farmerId) => {
     try {
       if (farmerId) {
-        const res = await api.post('/product/farmer-all-products', {
-           data:{farmerId} ,
-        });
+        const res = await api.post('/product/farmer-all-products', 
+           {
+        farmerId: farmerId,
+         }
+        );
         const items = res.data?.data || res.data;
         if (Array.isArray(items) && items.length > 0) return items;
       }
